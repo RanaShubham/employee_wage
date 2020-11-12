28d6520eb172ad67fd746e4aca937d166433dfa5
Shubham Rana <ranashubham7296@gmail.com>
2020-11-12 11:29:26 +0530

    Refactored: To add a static method to calculate wage

--- employeeWage.py
+++ employeeWage.py
@@ -1,25 +1,27 @@
 import random
+class EmployeeWage:
+    FULL_DAY_HRS = 8
+    WAGE_PER_HR = 20
+    PART_TIME_HRS = 4
+    MONTHLY_WORKING_DAYS = 20
+    TOTAL_WORKING_HRS = 100
+    ATTENDANCE = {
+        0 : (0,0),
+        1 : (FULL_DAY_HRS*WAGE_PER_HR, FULL_DAY_HRS),
+        2 : (PART_TIME_HRS*WAGE_PER_HR, PART_TIME_HRS)
+    }
 
-FULL_DAY_HRS = 8
-WAGE_PER_HR = 20
-PART_TIME_HRS = 4
-MONTHLY_WORKING_DAYS = 20
-TOTAL_WORKING_HRS = 100
+    @staticmethod
+    def calculateMonthlyWage():
+        monthlyWage = 0
+        totalWorkHours = 0
+        for i in range(1, EmployeeWage.MONTHLY_WORKING_DAYS, 1):
+            employeeAttendance = random.randint(0, 2)
+            if totalWorkHours + EmployeeWage.ATTENDANCE.get(employeeAttendance)[1] > EmployeeWage.TOTAL_WORKING_HRS:
+                break
+            monthlyWage = monthlyWage + EmployeeWage.ATTENDANCE.get(employeeAttendance)[0]
+            totalWorkHours = totalWorkHours + EmployeeWage. ATTENDANCE.get(employeeAttendance)[1]
+        return monthlyWage
 
-print ("Welcome to employee wage program")
-
-attendance = {
-    0 : (0,0),
-    1 : (FULL_DAY_HRS*WAGE_PER_HR, FULL_DAY_HRS),
-    2 : (PART_TIME_HRS*WAGE_PER_HR, PART_TIME_HRS)
-}
-monthlyWage = 0
-totalWorkHours = 0
-for i in range (1,MONTHLY_WORKING_DAYS,1):
-    employeeAttendance = random.randint(0, 2)
-    if totalWorkHours + attendance.get(employeeAttendance)[1]  > TOTAL_WORKING_HRS :
-        break
-    monthlyWage = monthlyWage + attendance.get(employeeAttendance)[0]
-    totalWorkHours = totalWorkHours + attendance.get(employeeAttendance)[1]
-
-print("Monthly salary is: ", monthlyWage)
+wage = EmployeeWage.calculateMonthlyWage()
+print(wage)
