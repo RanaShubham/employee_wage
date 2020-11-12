b2924cb818f80cbe9da99d9aa9092772c12c8b77
Shubham Rana <ranashubham7296@gmail.com>
2020-11-12 10:49:24 +0530

    Refactored: To calculate monthly salary of employee.

--- employeeWage.py
+++ employeeWage.py
@@ -3,13 +3,18 @@ import random
 FULL_DAY_HRS = 8
 WAGE_PER_HR = 20
 PART_TIME_HRS = 4
+MONTHLY_WORKING_DAYS = 20
 
 print ("Welcome to employee wage program")
-employeeAttendance = random.randint(0,2)
 
 attendance = {
     0 : 0,
     1 : FULL_DAY_HRS*WAGE_PER_HR,
     2 : PART_TIME_HRS*WAGE_PER_HR
 }
-print('Salary today is :  ', attendance.get(employeeAttendance))
+monthlyWage = 0
+for i in range (1,MONTHLY_WORKING_DAYS,1):
+    employeeAttendance = random.randint(0, 2)
+    monthlyWage = monthlyWage + attendance.get(employeeAttendance)
+
+print("Monthly salary is: ", monthlyWage)
