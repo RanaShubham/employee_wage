2c8df9141f3729ac341b67d9c5ae44bf6793bd28
Shubham Rana <ranashubham7296@gmail.com>
2020-11-12 17:49:02 +0530

    Added: Ability to create multiple comapnies.

--- employeeWage.py
+++ employeeWage.py
@@ -1,31 +1,45 @@
 import random
 class EmployeeWage:
 
-    def __init__(self, companyName,  wagePerHr,  partTimeHr,  fullTimeHr,  monthlyWorkingDays,  totalWorkingHrs):
-        self.companyName = companyName
-        self.wagePerHr = wagePerHr
-        self.partTimeHr = partTimeHr
-        self.fullTimeHr = fullTimeHr
-        self.monthlyWorkingDays = monthlyWorkingDays
-        self.totalWorkingHrs = totalWorkingHrs
-        self.attendanceType = { 0:(0,0), 1:(self.fullTimeHr*self.wagePerHr, self.fullTimeHr), 2:(self.partTimeHr*self.wagePerHr, self.partTimeHr) }
+    def __init__(self, company_name,  wage_per_hour,  part_time_hour,  full_time_hour,  monthly_working_days,  total_working_days):
+        self.company_name = company_name
+        self.wage_per_hour = wage_per_hour
+        self.part_time_hour = part_time_hour
+        self.full_time_hour = full_time_hour
+        self.monthly_working_days = monthly_working_days
+        self.total_working_days = total_working_days
+        self.attendance_type = { 0:0,
+                                 1: self.full_time_hour,
+                                 2: self.part_time_hour }
 
-    def calculateMonthlyWage(self):
-        monthlyWage = 0
-        totalWorkHours = 0
-        for i in range(1, self.monthlyWorkingDays, 1):
-            attendanceToday = random.randint(0, 2)
-            if totalWorkHours + self.attendanceType.get(attendanceToday)[1] > self.totalWorkingHrs:
+    def calculate_monthly_wage(self):
+        """
+        When called upon an EmployeeWage type object, calculates and returns monthly salary.
+        
+        :parameter
+        self
+        
+        :returns
+        Total wage at the end of month.
+        """
+        monthly_wage = 0
+        total_work_hrs = 0
+        
+        for i in range(1, self.monthly_working_days, 1):
+            
+            attendance_today = random.randint(0, 2)
+            if total_work_hrs + self.attendance_type.get(attendance_today) > self.total_working_days:
                 break
-            monthlyWage = monthlyWage + self.attendanceType.get(attendanceToday)[0]
-            totalWorkHours = totalWorkHours + self.attendanceType.get(attendanceToday)[1]
-        return monthlyWage
+            monthly_wage = monthly_wage + self.attendance_type.get(attendance_today) * self.wage_per_hour
+            total_work_hrs = total_work_hrs + self.attendance_type.get(attendance_today)
+            
+        return monthly_wage
 
-flipkartObject = EmployeeWage("Flipkart", 10, 6, 10, 25, 80)
-IBMObject = EmployeeWage("IBM", 20, 4, 8, 20, 100)
+flipkart_object = EmployeeWage("Flipkart", 25, 6, 10, 25, 80)
+ibm_object = EmployeeWage("IBM", 20, 4, 8, 20, 100)
 
-flipkartSalary = flipkartObject.calculateMonthlyWage()
-ibmSalary = IBMObject.calculateMonthlyWage()
+flipkart_salary = flipkart_object.calculate_monthly_wage()
+ibm_salary = ibm_object.calculate_monthly_wage()
 
-print(flipkartObject.companyName,'employee earned Rs: ', flipkartSalary)
-print(IBMObject.companyName, 'employee earned Rs: ',ibmSalary)
+print(flipkart_object.company_name,'employee earned Rs:',flipkart_salary,'for this month')
+print(ibm_object.company_name, 'employee earned Rs:',ibm_salary,'for this month')
