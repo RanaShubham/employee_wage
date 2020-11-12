ca6b7018fa067e7198f582e9138b84175cf3c0a4
Shubham Rana <ranashubham7296@gmail.com>
2020-11-12 09:37:40 +0530

    Added: Ability to calculate employee wage based on attendance

--- employeeWage.py
+++ employeeWage.py
@@ -1,5 +1,12 @@
 import random
-print ("Welcome to employee wage program")
 
+FULL_DAY_HRS = 8
+WAGE_PER_HR = 20
+
+print ("Welcome to employee wage program")
 employeeAttendance = random.randint(0,1)
-print(employeeAttendance)
+
+if employeeAttendance == 1:
+    print("Salary for today is: ", FULL_DAY_HRS*WAGE_PER_HR )
+else:
+    print("Salary for today is: ", 0)
