bcf61c627855079aa3a97625c4aab4f6321dc08f
Shubham Rana <ranashubham7296@gmail.com>
2020-11-12 09:51:01 +0530

    Addee: Ability for employee to work part time.

--- employeeWage.py
+++ employeeWage.py
@@ -2,11 +2,14 @@ import random
 
 FULL_DAY_HRS = 8
 WAGE_PER_HR = 20
+PART_TIME_HRS = 4
 
 print ("Welcome to employee wage program")
-employeeAttendance = random.randint(0,1)
+employeeAttendance = random.randint(0,2)
 
 if employeeAttendance == 1:
     print("Salary for today is: ", FULL_DAY_HRS*WAGE_PER_HR )
+elif employeeAttendance == 2:
+    print("Salary for today is: ", PART_TIME_HRS*WAGE_PER_HR)
 else:
-    print("Salary for today is: ", 0)
+    print("Salary for today is ", 0)
