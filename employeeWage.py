d98e735bc4de396cafbcc737846f9d8a8d4d3c8f
Shubham Rana <ranashubham7296@gmail.com>
2020-11-18 22:44:55 +0530

    [Shubham Rana] Refactored: To use **kwargs in constructor.

--- employeeWage.py
+++ employeeWage.py
@@ -1,27 +1,27 @@
 import json
 import random
 import functools
-from typing import List
+from typing import List, Dict, Any
 from com.bridgelabz.employeewage.employeeWageException import EmployeeWageException
 
 class EmployeeWage :
     #List to store various company objects.
     employee_wage_object_list = []
 
-    def __init__ (self, *args):
-        self . company_name = args[0]
-        self . wage_per_hour = args[1]
-        self . part_time_hour = args[2]
-        self . full_time_hour = args[3]
-        self . monthly_working_days = args[4]
-        self . max_working_hrs = args[5]
+    def __init__ (self, **kwargs):
+        self . company_name = kwargs ["company_name"]
+        self . wage_per_hour = kwargs ["wage_per_hour"]
+        self . part_time_hour = kwargs ["part_time_hour"]
+        self . full_time_hour = kwargs ["full_time_hour"]
+        self . monthly_working_days = kwargs ["monthly_working_days"]
+        self . max_working_hrs = kwargs ["max_working_hrs"]
         self . daily_wage_list = []
 
     def hours_worked_today(self):
         '''
-        To get the number of hours work was done today .
-        :return: integer signifying hours of work done today
-        :rtype: int
+            To get the number of hours work was done today .
+            :return: integer signifying hours of work done today
+            :rtype: int
         '''
         attendance_today = random . randint(0, 2)
         return 0 if attendance_today == 0\
@@ -30,9 +30,9 @@ class EmployeeWage :
 
     def create_daily_wage_list(self):
         '''
-        Creates a daily salary list of employee for the company .
-        :return: List of daily salaries .
-        :rtype: List
+            Creates a daily salary list of employee for the company .
+            :return: List of daily salaries .
+            :rtype: List
         '''
         work_hours_count = 0
         for _ in range(0, self . monthly_working_days, 1):
@@ -44,9 +44,9 @@ class EmployeeWage :
 
     def get_monthly_wage(self):
         '''
-        Calculates total wage from the wage list.
-        :return: Total wage
-        :rtype: int
+            Calculates total wage from the wage list.
+            :return: Total wage
+            :rtype: int
         '''
         self.create_daily_wage_list()
         return functools.reduce(lambda x, y : x + y, self . daily_wage_list)
@@ -54,28 +54,27 @@ class EmployeeWage :
     @staticmethod
     def get_emp_wage_object(company_dict):
         """
-        :param Dictionary of EmployeeWage  object data
-        :return: EmployeeWage object
+            :param Dictionary of EmployeeWage  object data
+            :return: EmployeeWage object
         """
         employee_wage_object = EmployeeWage(
-            company_dict.get("company_name"),
-            company_dict.get("wage_per_hour"),
-            company_dict.get("part_time_hour"),
-            company_dict.get("full_time_hour"),
-            company_dict.get("monthly_working_days"),
-            company_dict.get("max_working_hrs"),
-            company_dict.get("daily_wage_list")
-        )
+            company_name = company_dict.get("company_name"),
+            wage_per_hour = company_dict.get("wage_per_hour"),
+            part_time_hour = company_dict.get("part_time_hour"),
+            full_time_hour = company_dict.get("full_time_hour"),
+            monthly_working_days = company_dict.get("monthly_working_days"),
+            max_working_hrs = company_dict.get("max_working_hrs"),
+            daily_wage_list = company_dict.get("daily_wage_list"))
         return  employee_wage_object
 
     @staticmethod
     def get_data_from_file(str):
         '''
-        Parses through a json file to create a list of EmployeeWage type objects.
-        :param str: File path
-        :type str:  str
-        :return:  EmployeeWage object list.
-        :rtype:  list
+            Parses through a json file to create a list of EmployeeWage type objects.
+            :param str: File path
+            :type str:  str
+            :return:  EmployeeWage object list.
+            :rtype:  list
         '''
         if not str.lower().endswith('.json'):
             raise EmployeeWageException("Not a json file.")
@@ -93,9 +92,9 @@ class EmployeeWage :
 
 def driver_function(file):
     '''
-    Prints the total monthly wage for employees of different companies.
-    :param file:
-    :type file:
+        Prints the total monthly wage for employees of different companies.
+        :param file:
+        :type file:
     '''
     for emp_wage_obj in EmployeeWage.get_data_from_file(file):
         print("{} employee earned Rs {}".format(emp_wage_obj.company_name, emp_wage_obj.get_monthly_wage()))
--- test_employeewage/test_employeewage.py
+++ test_employeewage/test_employeewage.py
@@ -3,9 +3,32 @@ import pytest
 from com.bridgelabz.employeewage.employeeWage import EmployeeWage
 from com.bridgelabz.employeewage.employeeWageException import EmployeeWageException
 
-empwage = EmployeeWage("xyz", 10, 5, 10, 20, 100)
-empwage_large_working_hrs = EmployeeWage("xyz", 10, 5, 10, 20, 1000000)
-empwage_large_working_days = EmployeeWage("xyz", 10, 5, 10, 1000000, 100)
+empwage = EmployeeWage(
+    company_name="xyz",
+    wage_per_hour=10,
+    part_time_hour=5,
+    full_time_hour=10,
+    monthly_working_days=20,
+    max_working_hrs=100)
+
+empwage_large_working_hrs = EmployeeWage(
+     company_name="xyz",
+     wage_per_hour=10,
+     part_time_hour=5,
+     full_time_hour=10,
+     monthly_working_days=20,
+     max_working_hrs=100000000)
+
+empwage_large_working_days = EmployeeWage(
+    company_name="xyz",
+    wage_per_hour=10,
+    part_time_hour=5,
+    full_time_hour=10,
+    monthly_working_days=100000000,
+    max_working_hrs=20)
+
+absent_file = './NoFile.json'
+wrong_type_file = './WrongType.txt'
 
 def test_hours_worked_today():
     for _ in range(1, 20, 1):
@@ -28,10 +51,10 @@ def test_get_monthly_wage_when_large_maximum_working_days_should_return_sal_less
 
 def test_get_data_file_when_json_file_to_read_company_data_not_found_should_raise_EmployeeWageException():
     with pytest.raises(EmployeeWageException) as error_type:
-        EmployeeWage.get_data_from_file('./WrongType.txt')
+        EmployeeWage.get_data_from_file(wrong_type_file)
     assert  str(error_type.value) == "Not a json file."
 
 def test_get_data_file_when_json_type_but_not_present_should_raise_EmployeeWageException():
     with pytest.raises(EmployeeWageException) as error_type:
-        EmployeeWage.get_data_from_file('./NoFile.json')
+        EmployeeWage.get_data_from_file(absent_file)
     assert  str(error_type.value) == "File not found."
